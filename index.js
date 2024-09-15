2f6ee98c27a55c6c0ab91f253e601a65519a1677
Maria Shevchenko <mariassheva@gmail.com>
2024-09-15 14:18:51 +0300

    task 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var fs = require('fs');
 
 const PORT = 3000;
 
@@ -25,6 +26,54 @@ app.get('/register', (req,res) =>{
     res.sendFile(path.resolve(__dirname,'html', 'register.html'));
 })
 
+app.get('/submit-register', (req, res) => {
+    console.log('register');
+    fs.writeFile('inf.txt', `attempt to register!`, (err, data)  => {
+        if (err) throw err;
+    })
+})
+
+app.get('/submit-sign-in', (req, res) => {
+    fs.writeFile('inf.txt', `attempt to sign in!`, (err, data)  => {
+        if (err) throw err;
+    })
+})
+
+var buff = '';
+fs.readFile('products.txt',  (err, data) => {
+    if (err) throw err;
+    buff = JSON.parse(data);
+    //console.log(buff);
+});
+
+app.get('/name/:nameId/price/:priceId', function(request, response){
+    console.log(`price: ${request.params['priceId']}`);
+    var name = request.params['nameId'];
+    var price = request.params['priceId'];
+    
+    //поиск по названию в buff и отображение других данных, которые относятся к этому названию
+    var product = buff.find(item => item.name == name);
+
+    if (product) {
+        if (product.price === price) {
+            response.send(`
+                <h1>Product Details</h1>
+                <p><strong>Name:</strong> ${product.name}</p>
+                <p><strong>Price:</strong> ${product.price}</p>
+                <p><strong>CPU:</strong> ${product.CPU}</p>
+            `);
+        } else {
+            response.send('Price does not match the product');
+        }
+    } else {
+        response.send('Product not found');
+    }
+
+
+});
+
 app.listen(PORT, ()=>{
     console.log(`Server has been started on port ${PORT} ...`);
 });
+
+///     name/Dell XPS 13/price/$999
